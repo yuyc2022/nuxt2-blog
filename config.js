f17efcebf782cc7b925a330f193d589418cddb1d
yuyc2022 <110431729+yuyc2022@users.noreply.github.com>
2022-08-02 11:40:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
+  title: 'yuyichen的博客', // 网站标题
+  nickName: "yuyichen", // 昵称
   githubName: 'yuyc2022', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: 'yuyichen的博客，书写值得书写的东西',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,yuyichen的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
